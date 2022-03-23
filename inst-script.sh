e84eaba11a4d4d28910ca6f04ca5d704a4f9d8b1
Tyler887 <tylermageeshields@gmail.com>
2022-03-23 21:32:57 +0000

    Update inst-script.sh

--- inst-script.sh
+++ inst-script.sh
@@ -1,2 +1,2 @@
 echo "Installing dependencies (may request user password due to sudo)..."
-sudo apt install python python3-* git -y
+sudo apt-get install python3 python3-requests python3-colorama python3-pip git -y # apt does not have a stable CLI interface. Use with caution in scripts.
