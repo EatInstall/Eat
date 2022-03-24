a843ba62911ed48f0569149be5b8a75907e41455
Tyler887 <tylermageeshields@gmail.com>
2022-03-24 19:10:27 +0000

    Update inst-script.sh

--- inst-script.sh
+++ inst-script.sh
@@ -1,3 +1,4 @@
+# license: GPLv3+: GPL version 3 or later
 echo "Installing dependencies (may request user password due to sudo)..."
 sudo apt-get install python3 python-is-python3 python3-requests python3-colorama python3-pip git -y # apt does not have a stable CLI interface. Use with caution in scripts.
 echo "Downloading eat..."
