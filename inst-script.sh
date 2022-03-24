e5dd7723cc2d4660ee42312e32885371fab3d0d1
Tyler887 <tylermageeshields@gmail.com>
2022-03-24 15:46:05 +0000

    Update inst-script.sh

--- inst-script.sh
+++ inst-script.sh
@@ -3,5 +3,5 @@ sudo apt-get install python3 python-is-python3 python3-requests python3-colorama
 echo "Downloading eat..."
 git clone https://github.com/Tyler887/eat.git Eat-PKG-Manager --single-branch 1.0
 echo "Symlinking a binary to python script..."
-echo "#!/usr/bin/bash\npython3 ~/Eat-PKG-Manager/eat.py" >> /usr/bin/eat
-echo "Please restart your she
+sudo echo "#!/usr/bin/bash\npython3 ~/Eat-PKG-Manager/eat.py" >> /usr/bin/eat
+echo "Please restart your shell to apply the changes!"
