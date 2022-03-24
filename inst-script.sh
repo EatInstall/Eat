66f4b4cc8e7bc19df9367d5f23a2955824676288
Tyler887 <tylermageeshields@gmail.com>
2022-03-24 15:46:51 +0000

    Update inst-script.sh

--- inst-script.sh
+++ inst-script.sh
@@ -1,7 +1,7 @@
 echo "Installing dependencies (may request user password due to sudo)..."
 sudo apt-get install python3 python-is-python3 python3-requests python3-colorama python3-pip git -y # apt does not have a stable CLI interface. Use with caution in scripts.
 echo "Downloading eat..."
-git clone https://github.com/Tyler887/eat.git Eat-PKG-Manager --single-branch 1.0
+git clone https://github.com/Tyler887/eat.git Eat-PKG-Manager
 echo "Symlinking a binary to python script..."
 sudo echo "#!/usr/bin/bash\npython3 ~/Eat-PKG-Manager/eat.py" >> /usr/bin/eat
 echo "Please restart your shell to apply the changes!"
