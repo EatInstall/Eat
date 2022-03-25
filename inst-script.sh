b8127e403d9ef40ea8cfd72cdead9bb3ea282f2c
Tyler887 <tylermageeshields@gmail.com>
2022-03-25 18:24:09 +0000

    Update inst-script.sh

--- inst-script.sh
+++ inst-script.sh
@@ -4,11 +4,6 @@ sudo apt-get install python3 python-is-python3 python3-requests python3-colorama
 echo "Downloading eat..."
 git clone https://github.com/Tyler887/eat.git ~/Eat-PKG-Manager
 echo "Symlinking eat command to the python script..."
-mkdir ~/EatPKGSymlink
-echo -n "" >> ~/EatPKGSymlink/eat # new Symlink
-echo "#!/usr/bin/bash" >> ~/EatPKGSymlink/eat
-echo "python3 ~/Eat-PKG-Manager/eat.py" >> ~/EatPKGSymlink/eat
-echo "Forcing BASH to add Eat to the path..."
 echo "" >> ~/.bashrc
 echo "# Add eat package manager to the path." >> ~/.bashrc
 echo "alias eat='python ~/EatPKGManager/eat.py'" >> ~/.bashrc
