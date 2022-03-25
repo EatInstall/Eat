38dd4428da7c63d988348e538305adc326a332a3
Tyler887 <tylermageeshields@gmail.com>
2022-03-25 21:25:04 +0000

    Update inst-script.sh

--- inst-script.sh
+++ inst-script.sh
@@ -1,6 +1,6 @@
 # license: GPLv3+: GPL version 3 or later
 echo "Installing dependencies (may request user password due to sudo)..."
-sudo apt-get install python3 python-is-python3 python3-requests python3-colorama python3-pip git -y # apt does not have a stable CLI interface. Use with caution in scripts.
+sudo apt-get install python3 python-is-python3 python3-requests python3-colorama python3-pip python3-pyyaml git -y # apt does not have a stable CLI interface. Use with caution in scripts.
 echo "Downloading eat..."
 git clone https://github.com/Tyler887/eat.git ~/Eat-PKG-Manager
 echo "Symlinking eat command to the python script..."
