a3b656b11e76d8c4f68ff7a0767546d0b9c4d1aa
Tyler887 <tylermageeshields@gmail.com>
2022-03-25 22:03:22 +0000

    Update eat-install.py

--- eat-install.py
+++ eat-install.py
@@ -5,12 +5,12 @@ def import_modules():
     import sys as c
     import colorama
     import urllib
-    import zipfile
+    import zipfile, tarfile
     import yaml # PyYAML
-    import hashlib
+    import shutil
 import_modules()
 
-parser = argparse.ArgumentParser(prog="Eat Utilities", usage="install-eat-package target [options]", elipog="This program may be freely distributed under the terms of GPLv3 or any later version.")
+parser = argparse.ArgumentParser(prog="Eat Utilities", usage="eatinst target [options]", elipog="This program may be freely distributed under the terms of GPLv3 or any later version.")
 
 parser.add_argument('target', type=str,
                     help='package to install')
@@ -49,23 +49,26 @@ with open(f"~/eat_sources/{args.target}.yaml", "r") as manifest:
   global packageUri
   global packageVersion
   global packageRequiresAdmin
-  global packageNotes
   global packageSuggestions
   global packageRequirements
   convertedManifest = yaml.full_load(manifest.read()) # Convert YAML manifest to Python dictionary
   packageUri = convertedManifest['uri']
-  if not packageUri.endswith(".zip"):
-      print(f"{Fore.RED}Error:{Style.RESET_ALL} Only compressed packages are compatible with eat at the moment.")
+  if not packageUri.endswith(".zip") and not packageUri.endswith(".tar.gz"):
+      print(f"{Fore.RED}Error:{Style.RESET_ALL} Only zip and gzip-tarred packages are compatible with eat at the moment.")
+      shutil.rmtree("~/eat_sources")
       exit(1)
   packageRequiresAdmin = convertedManifest['sudo_necessary']
   if packageRequiresAdmin and posix_tools.geteuid() != 0:
+     shutil.rmtree("~/eat_sources")
      print(f"{Fore.RED}Error:{Style.RESET_ALL} Installing this package requires root access, maybe try with sudo?")
      exit(1)
   packageRequirements = convertedManifest['depends']
   for i in packageRequirements:
      if not posix_tools.path.isdir(f"~/Eat-Apps/{i}"):
+        shutil.rmtree("~/eat_sources")
         print(f"{Fore.RED}Error:{Style.RESET_ALL} This package requires other packages in order to function. Please install them and try again.\nThe first package detected was: {i}")
         exit(1)
+  packageSuggestions = convertedManifest['should_install']
   for i in packageSuggestions:
       print(f"{Fore.YELLOW}Warning:{Style.RESET_ALL} The following package is recommended for {args.target}: {i}")
   url = packageUri
@@ -79,7 +82,24 @@ with open(f"~/eat_sources/{args.target}.yaml", "r") as manifest:
     print(f"Downloaded {args.target}!")
   else:
     print("Extracting to app directory.")
-    with zipfile.ZipFile(f"~/eat_pack_{args.target}.zip", 'r') as zip_ref:
-      zip_ref.extractall(f"~/eat_app_{args.target}")
-    posix_tools.system(f"export PATH=~/eat_pack_{args.target}:$PATH")
+    if url.endswith(".zip"): # Zipped
+      with zipfile.ZipFile(f"~/eat_pack_{args.target}.zip", 'r') as zip_ref:
+        zip_ref.extractall(f"~/eat_app_{args.target}")
+      posix_tools.unlink(f"~/eat_pack_{args.target}.zip")
+    else: # Tarred, uses same engine as zip, see https://stackoverflow.com/questions/39265680/how-to-convert-tar-gz-file-to-zip-using-python-only
+      tarf = tarfile.open( name=f'eat_pack_{args.target}.tar.gz', mode='r|gz' )
+      zipf = zipfile.ZipFile( file=f'~/eat_pack_{args.target}.zip', mode='a', compression=zipfile.ZIP_DEFLATED )
+      for m in tarf:
+        f = tarf.extractfile( m )
+        fl = f.read()
+        fn = m.name
+      zipf.writestr( fn, fl )
+      tarf.close()
+      zipf.close()
+      with zipfile.ZipFile(f"~/eat_pack_{args.target}.zip", 'r') as zip_ref:
+        zip_ref.extractall(f"~/eat_app_{args.target}")
+      posix_tools.unlink(f"~/eat_pack_{args.target}.zip")
+      posix_tools.unlink(f"~/eat_pack_{args.target}.tar.gz")
+    posix_tools.system(f"export PATH=~/eat_app_{args.target}:$PATH")
     print(f"Installed {args.target}!")
+shutil.rmtree("~/eat_sources")
