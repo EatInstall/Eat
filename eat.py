bb6131e398556136f6ad0d1a1a04f76f16edbfc8
Tyler887 <tylermageeshields@gmail.com>
2022-05-17 22:18:37 +0100

    Delete eat.py

--- eat.py
+++
@@ -1,7 +0,0 @@
-#############################
-#      DO NOT EDIT THIS     #
-#############################
-# Only edit if making a
-# fork of eat with the
-# name renamed.
-print("Please use an actual eat tool, such as eatinst.")
